c7661e96305008c7cc8d5723a2a409ff478d2177
rlnsy <rowlindsay@gmail.com>
2020-05-03 13:28:19 -0700

    fix one-to-one warning

--- application/game_schema/games/models/games_players.py
+++ application/game_schema/games/models/games_players.py
@@ -12,7 +12,7 @@ PLAYER_TOKEN_LENGTH = 50
 PLAYER_TOKEN_CHARS = [c for c in string.printable if c not in string.whitespace]
 
 class Player(models.Model):
-    agent_id = models.ForeignKey(Agent, on_delete=models.CASCADE, primary_key=True)
+    agent_id = models.OneToOneField(Agent, on_delete=models.CASCADE, primary_key=True)
     display_name = models.CharField(max_length=50)
     token = models.CharField(max_length=50, blank=False, null=False) # fixed length 50
     def __str__(self):
