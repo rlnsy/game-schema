171a6cf16459808f9fed3b7696954282459b370a
rlnsy <rowlindsay@gmail.com>
2020-05-03 13:22:11 -0700

    move views

--- application/game_schema/games/urls.py
+++ application/game_schema/games/urls.py
@@ -1,26 +1,9 @@
 from django.urls import path
 
-
-from rest_framework import generics, serializers
-from rest_framework.views import APIView, Response
-from rest_framework.permissions import AllowAny
-from games.models import Game
-
-class GameSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Game
-        fields = ['id']
-
-class GameListAPIView(generics.ListAPIView):
-    queryset = Game.objects.all()   
-    serializer_class = GameSerializer  # show all details
-    permission_classes = [AllowAny]
-
-
-from .players import create_player
-class PlayerAPIView(APIView):
-    def post(self, request):
-        return create_player(request)
+from .views import (
+    GameListAPIView,
+    PlayerAPIView
+)
 
 
 urlpatterns = [
--- application/game_schema/games/views.py
+++ application/game_schema/games/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, serializers
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from games.models import Game
 
-# Create your views here.
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Game
+        fields = ['id']
+
+class GameListAPIView(generics.ListAPIView):
+    queryset = Game.objects.all()   
+    serializer_class = GameSerializer  # show all details
+    permission_classes = [AllowAny]
+
+
+from .players import create_player
+class PlayerAPIView(APIView):
+    def post(self, request):
+        return create_player(request)
