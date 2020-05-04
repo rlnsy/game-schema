d1994498b2cd09753c71595c76d3470f474b5287
rlnsy <rowlindsay@gmail.com>
2020-05-04 12:22:29 -0700

    move adopt role header

--- application/game_schema/games/api/external/session.py
+++ application/game_schema/games/api/external/session.py
@@ -31,16 +31,3 @@ def create_session(request):
                 get_by_id(Game, d['game_id'], 
             lambda g: 
                 create_game_session(p, g))))
-
-
-# from games.models.games_session import HasRole
-
-# def adopt_role(agent, role, session):
-#     def create():
-#         record = HasRole(
-#             session_id=session,
-#             agent_id=agent,
-#             role_id=role,
-#             is_active=True )
-#         record.save()
-#     return 
---
+++ application/game_schema/games/api/internal/exceptions.py
@@ -0,0 +1,2 @@
+class AlreadyHasRole(Exception):
+    pass
