a6cd4b7e8829d685996ceb2cd8c3540c8c6036c5
rlnsy <rowlindsay@gmail.com>
2020-05-04 12:51:26 -0700

    bump update

--- application/game_schema/games/api/internal/session.py
+++ application/game_schema/games/api/internal/session.py
@@ -1,11 +1,11 @@
 from games.models.games_session import HasRole
 
-# def adopt_role(agent, role, session):
-#     def create():
-#         record = HasRole(
-#             session_id=session,
-#             agent_id=agent,
-#             role_id=role,
-#             is_active=True )
-#         record.save()
-#     return 
+def adopt_role(agent, role, session):
+    def create():
+        record = HasRole(
+            session_id=session,
+            agent_id=agent,
+            role_id=role,
+            is_active=True )
+        record.save()
+    # return 
