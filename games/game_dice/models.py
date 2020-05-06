ab47b1493bec13d0fe134b51ef95d48385c23568
rlnsy <rowlindsay@gmail.com>
2020-05-05 20:50:32 -0700

    action relation as model inheritance and type model

--- games/game_dice/models.py
+++ games/game_dice/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from games.models.games_core import Action
 
 
-class Roll(models.Model):
+class Roll(Action):
     die_1_value = models.IntegerField()
     die_2_value = models.IntegerField()
--- games/models/games_core.py
+++ games/models/games_core.py
@@ -12,8 +12,11 @@ class Role(models.Model):
     def __str__(self):
         return "role: %s" % self.id
 
-class Action(models.Model):
+class ActionType(models.Model):
+    type_id = models.CharField(max_length=100, primary_key=True)
     game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
-    subtype_id = models.CharField(max_length=100, null=False)
     def __str__(self):
-        return "action (%s): %s" % (self.game_id, self.subtype_id)
+        return "%s:%s" % (self.game_id, self.type_id)
+
+class Action(models.Model):
+    action_type = models.ForeignKey(ActionType, on_delete=models.CASCADE)
