9fcfd281a5c5251c8ea51c539ac6022c98ad80b8
rlnsy <rowlindsay@gmail.com>
2020-05-05 22:01:19 -0700

    change roll action attributes

--- games/game_dice/models.py
+++ games/game_dice/models.py
@@ -3,5 +3,6 @@ from games.models.games_core import Action
 
 
 class Roll(Action):
-    die_1_value = models.IntegerField()
-    die_2_value = models.IntegerField()
+    #die_1_value = models.IntegerField()
+    #die_2_value = models.IntegerField()
+    num_shakes = models.IntegerField()
