d62e383d7ac913225a4196d40f354bc4842baaa7
rlnsy <rowlindsay@gmail.com>
2020-05-11 14:33:47 -0700

    fix regex

--- games/api/external/urls.py
+++ games/api/external/urls.py
@@ -28,7 +28,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    re_path('swagger(?P<format>\\.json|\\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path('^swagger(?P<format>\\.json|\\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('games', GameListAPIView.as_view(), name='game-list'),
