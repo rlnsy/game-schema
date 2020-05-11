d39d08685f45ac238b77c663e0c7948e9961f73c
rlnsy <rowlindsay@gmail.com>
2020-05-11 14:12:06 -0700

    update api spec info

--- games/api/external/urls.py
+++ games/api/external/urls.py
@@ -15,11 +15,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Games Schema API",
       default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
+      description="Front-end for manipulating the system",
+      terms_of_service="https://github.com/rlnsy/games-schema/blob/master/README.md",
+      contact=openapi.Contact(email="dev.gmbx@yahoo.com"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
