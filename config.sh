80817d33cf36e1d5879fc93bdddfbd0084faf54a
rlnsy <rowlindsay@gmail.com>
2020-05-20 05:27:23 +0000

    nginx ssl configuration for frontend

--- config.sh
+++ config.sh
@@ -3,5 +3,10 @@
 cp frontend/.env.production frontend/.env.production.local
 "$EDITOR" frontend/.env.production.local
 
+cp frontend/nginx.conf frontend/nginx.local.conf
+"$EDITOR"  frontend/nginx.local.conf
+
 cp app/deployment.json app/deployment.local.json
 "$EDITOR"  app/deployment.local.json
+
+echo "Put SSL cert and key in ssl directory and everything should be good"
